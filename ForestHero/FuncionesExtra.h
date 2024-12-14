88a491fd7bdbd7e1536e681ea3a1d0325d06558e
Jahat Trinidad <jahatjassiel@gmail.com>
2024-12-13 21:10:32 -0500

    Mejora en la clase Agente

--- ForestHero/FuncionesExtra.h
+++ ForestHero/FuncionesExtra.h
@@ -7,7 +7,115 @@
 using namespace std;
 using namespace System;
 
+struct Color
+{
+    BYTE r;
+    BYTE g;
+    BYTE b;
+};
+
+// Basado en la paleta de Catpuccin
+constexpr Color NEGRO = { 36, 39, 58 };
+constexpr Color BLANCO = { 202, 211, 245 };
+constexpr Color ROJO = { 237, 135, 150 };
+constexpr Color VERDE = { 166, 218, 149 };
+constexpr Color AMARILLO = { 238, 212, 159 };
+constexpr Color AZUL = { 138, 173, 244 };
+constexpr Color MORADO = { 198, 160, 246 };
+constexpr Color NARANJA = { 245, 169, 127 };
+constexpr Color CIELO = { 145, 215, 227 };
+constexpr Color MARRON = { 238, 153, 160 };
+constexpr Color GRIS = { 147, 154, 183 };
+constexpr Color CELESTE = { 125, 196, 228 };
+constexpr Color AGUAROSA = { 244, 219, 214 };
+constexpr Color LAVANDA = { 183, 189, 248 };
+constexpr Color FLAMENCO = { 145, 215, 227 };
+constexpr Color OSCURIDAD = { 24, 25, 38 };
+
 void GoTo(int x, int y) {
     COORD coord = { x, y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
+void setFont(const wchar_t* fontName, SHORT sizeX, SHORT sizeY) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(CONSOLE_FONT_INFOEX);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = sizeX; // Ancho de cada caracter
+    cfi.dwFontSize.Y = sizeY; // Alto de cada caracter
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_NORMAL;
+    wcscpy_s(cfi.FaceName, fontName);
+    SetCurrentConsoleFontEx(hConsole, FALSE, &cfi);
+}
+
+void setColorPalette(int index, Color color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_SCREEN_BUFFER_INFOEX csbiex;
+    csbiex.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
+    GetConsoleScreenBufferInfoEx(hConsole, &csbiex);
+
+    // Establecer el color en la paleta
+    csbiex.ColorTable[index] = RGB(color.r, color.g, color.b);
+
+    SetConsoleScreenBufferInfoEx(hConsole, &csbiex);
+}
+
+void hideCursor() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+
+    GetConsoleCursorInfo(hConsole, &cursorInfo);
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(hConsole, &cursorInfo);
+}
+
+void setBkgTxtColor(int colorTexto, int colorFondo) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorTexto | (colorFondo << 4));
+}
+
+void configurarConsola() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    hideCursor();
+
+    setFont(L"Cascadia Mono", 10, 20);
+
+    setColorPalette(0, NEGRO);
+    setColorPalette(1, BLANCO);
+    setColorPalette(2, VERDE);
+    setColorPalette(3, CIELO);
+    setColorPalette(4, ROJO);
+    setColorPalette(5, MORADO);
+    setColorPalette(6, AMARILLO);
+    setColorPalette(7, NARANJA);
+    setColorPalette(8, AZUL);
+    setColorPalette(9, MARRON);
+    setColorPalette(10, GRIS);
+    setColorPalette(11, CELESTE);
+    setColorPalette(12, AGUAROSA);
+    setColorPalette(13, LAVANDA);
+    setColorPalette(14, FLAMENCO);
+    setColorPalette(15, OSCURIDAD);
+
+    setBkgTxtColor(1, 0);
+}
+
+/*
+>>>>>>> FUNCIONES SIN USAR
+
+void configurarColorTexto(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void configurarColorFondoIntensoYTexto(int colorTexto, int colorFondo) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorTexto | (colorFondo << 4) | BACKGROUND_INTENSITY);
+}
+
+void configurarColorFondoYTextoIntensos(int colorTexto, int colorFondo) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorTexto | (colorFondo << 4) | FOREGROUND_INTENSITY | BACKGROUND_INTENSITY);
+}
+*/
